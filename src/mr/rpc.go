991cb4ace6a72686502bf863fe58d5ac3d24b825
emot1con <arqanpurusa@gmail.com>
2026-09-04 19:48:38 +0700

    initialized commit

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -21,3 +21,31 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type GetTaskArgs struct { }
+
+type GetTaskReply struct {
+	IDTask int
+	TaskType string
+	MapTask *MapTask
+	ReduceTask *ReduceTask
+}
+
+type ReportTaskArgs struct { 
+	IDTask int
+	TaskType string
+}
+
+type ReportTaskReply struct {
+	Status string
+}
+
+type MapTask struct {
+	IDMapTask int
+	FileName string
+	ReduceTaskNum int
+}
+
+type ReduceTask struct {
+	IDReduceTask int
+	MapTaskNum int
+}
