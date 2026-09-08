eb4bc81f88a0aaa735d63ac21cca47c72545ab93
emot1con <arqanpurusa@gmail.com>
2026-09-08 11:49:59 +0700

    added worker map task process

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,11 +1,14 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-import "os"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"os"
+	"time"
+)
 
 // Map functions return a slice of KeyValue.
 type KeyValue struct {
@@ -23,7 +26,6 @@ func ihash(key string) int {
 
 var coordSockName string // socket for coordinator
 
-
 // main/mrworker.go calls this function.
 func Worker(sockname string, mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
@@ -31,10 +33,99 @@ func Worker(sockname string, mapf func(string, string) []KeyValue,
 	coordSockName = sockname
 
 	// Your worker implementation here.
+	for {
+		reply, ok := getTask()
+		if !ok {
+			return
+		}
+
+		switch reply.TaskType {
+		case "exit":
+			return
+		case "wait":
+			time.Sleep(1 * time.Second)
+			continue
+		case "map":
+			if err := doMapTask(reply.MapTask, mapf); err != nil {
+				fmt.Printf("Map task failed: %v\n", err)
+				continue
+			}
+			if _, ok := reportTask(reply.IDTask, "map"); !ok {
+				fmt.Printf("Recuce Task with ID: %v is fail", reply.TaskType)
+				continue
+			}
+
+		case "reduce":
+
+			if _, ok := reportTask(reply.IDTask, "reduce"); !ok {
+				fmt.Printf("Recuce Task with ID: %v is fail", reply.TaskType)
+				continue
+			}
+		default:
+			fmt.Println("Theres Something Wrong, Trying Again")
+			continue
+		}
+	}
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+}
+
+func getTask() (*GetTaskReply, bool) {
+	args := new(GetTaskArgs)
+	reply := new(GetTaskReply)
+	return reply, call("Coordinator.GetTask", &args, &reply)
+}
+
+func reportTask(id int, taskType string) (*ReportTaskReply, bool) {
+	reportArgs := new(ReportTaskArgs)
+	reportArgs.IDTask = id
+	reportArgs.TaskType = taskType
+
+	reportReply := new(ReportTaskReply)
+
+	return reportReply, call("Coordinator.ReportTask", &reportArgs, &reportReply)
+}
+
+func doMapTask(task *MapTask, mapf func(string, string) []KeyValue) error {
+	content, err := os.ReadFile(task.FileName)
+	if err != nil {
+		fmt.Printf("File %s with Map Task ID: %v is fail to open", task.FileName, task.IDMapTask)
+	}
+	kva := mapf(task.FileName, string(content))
+	buckets := make([][]KeyValue, task.ReduceTaskNum)
+
+	for i := range kva {
+		r := ihash(kva[i].Key) % task.ReduceTaskNum
+		buckets[r] = append(buckets[r], kva[i])
+	}
+
+	for i := range buckets {
+		tmpFile, err := os.CreateTemp(".", "mr-temp-*")
+		if err != nil {
+			fmt.Printf("File %s with Map Task ID: %v is fail to open", task.FileName, task.IDMapTask)
+			return err
+		}
+		enc := json.NewEncoder(tmpFile)
+		
+		for _, kv := range buckets[i] {
+			if err := enc.Encode(&kv); err != nil {
+				tmpFile.Close()
+				os.Remove(tmpFile.Name())
+				return err
+			}
+		}
+		tmpFile.Close()
+
+		// 3. Rename ke nama file final: mr-IDMapTask-i
+		oname := fmt.Sprintf("mr-%d-%d", task.IDMapTask, i)
+		if err := os.Rename(tmpFile.Name(), oname); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // example function to show how to make an RPC call to the coordinator.
