c4c49d7d47c66a778154ddd6adf0ea3e45a00093
emot1con <arqanpurusa@gmail.com>
2026-09-26 12:09:40 +0700

    add acquire & release mechanism

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,6 +1,11 @@
 package lock
 
 import (
+	"math/rand/v2"
+	"strconv"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 )
 
@@ -23,14 +28,35 @@ type Lock struct {
 // independent.
 func MakeLock(ck kvtest.IKVClerk, lockname string) *Lock {
 	lk := &Lock{ck: ck}
-	// You may add code here
+	lk.LockName = lockname
+	lk.ClientID = strconv.FormatInt(rand.Int64(), 10)
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		val, ver, rep := lk.ck.Get(lk.LockName)
+		if rep == rpc.OK && val == "" {
+			if rep := lk.ck.Put(lk.LockName, lk.ClientID, ver); rep == rpc.OK {
+				return
+			}
+		}
+
+		if rep == rpc.ErrNoKey {
+			if rep := lk.ck.Put(lk.LockName, lk.ClientID, 0); rep == rpc.OK {
+				return
+			}
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	if val, ver, _ := lk.ck.Get(lk.LockName); val == lk.ClientID {
+		if rep := lk.ck.Put(lk.LockName, "", ver); rep == rpc.OK {
+			return
+		}
+	}
 }
